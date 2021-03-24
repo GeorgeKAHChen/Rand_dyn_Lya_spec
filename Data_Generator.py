e242b549e8e003ced2a77c97bea1f88ef6c679b7
GeorgeKahChen <GeorgeKahChen@gmail.com>
2021-03-24 19:29:10 +0800

    fixed multi processing memory bug

--- Data_Generator.py
+++ Data_Generator.py
@@ -11,9 +11,8 @@
 
 #from model import Logistic as model
 #from model import Henon as model
-#from model import Lorenz as model
-#from model import Lorenz as model
-from model import Rossler as model
+from model import Lorenz as model
+#from model import Rossler as model
 #from model import Ikeda as model
 #==================================================
 
